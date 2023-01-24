a0879423129906e3be6cc0a4f5e3149680c26de9
Pavel Artsishevsky <polter.rnd@gmail.com>
2023-01-24 21:30:09 +0300

    firefox: update, add comments
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- firefox/user.js
+++ firefox/user.js
@@ -1,16 +1,27 @@
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.tabs.warnOnCloseOtherTabs", false);
-user_pref("dom.push.connection.enabled", false);
-user_pref("dom.push.enabled", false);
-user_pref("dom.webnotifications.enabled", false);
-user_pref("dom.webnotifications.serviceworker.enabled", false);
-user_pref("gfx.webrender.compositor", true);
-user_pref("gfx.webrender.compositor.force-enabled", true);
-user_pref("gfx.webrender.enabled", true);
-user_pref("layers.acceleration.force-enabled", true);
-user_pref("layers.force-active", true);
 user_pref("ui.key.menuAccessKeyFocuses", false);
-user_pref("widget.wayland-dmabuf-vaapi.enabled", true);
-user_pref("widget.wayland-dmabuf-webgl.enabled", true);
+/* Accelerated canvas */
+user_pref("gfx.canvas.accelerated", true);
+/* WebRender enable */
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("gfx.webrender.enable-capture", true);
+user_pref("gfx.webrender.compositor.force-enabled", true);
+/* Accelerated Compositing Layers */
+user_pref("layers.acceleration.force-enabled", true);
+/* VAAPI */
 user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.ffvpx.enabled", false);
+user_pref("media.navigator.mediadatadecoder_vpx_enabled", true);
+/* For userChrome.css */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+/* Hide annoying WebRTC indicator */
+user_pref("privacy.webrtc.legacyGlobalIndicator", false);
+user_pref("privacy.webrtc.hideGlobalIndicator", true);
+
+/* Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1619585 */
+/*user_pref("security.sandbox.content.level", 0);
+user_pref("media.rdd-process.enabled", false);
+user_pref("media.rdd-ffvpx.enabled", false);
+user_pref("media.rdd-vpx.enabled", false);*/
